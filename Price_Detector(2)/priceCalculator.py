431532dbfaf73bf24114670219bb415d000830e3
andrei <mafteiandreiiulian@gmial.com>
2025-11-22 00:29:25 +0100

    andrei first push

---
+++ Price_Detector(2)/priceCalculator.py
@@ -0,0 +1,13 @@
+
+
+def get_all_prices(prices_dict):
+    """
+    Extracts all price values from a dictionary of prices.
+
+    Args:
+        prices_dict (dict): A dictionary where keys are price identifiers and values are price amounts.
+
+    Returns:
+        list: A list of all price values.
+    """
+    return list(prices_dict.values())
